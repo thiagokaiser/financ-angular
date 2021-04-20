3083006dc4ba313b0d8654788cfc76da30eeef90
Thiago Kaiser <thiago.kaiser@hotmail.com>
2021-04-20 17:47:14 -0300

    Estrutura Home

--- src/app/financ/home/home.component.ts
+++ src/app/financ/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild("barChart", { static:true }) elemento: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+
+    new Chart("barChart", {
+      type: 'bar',
+      data: {
+          labels: ["1","2","3","4","5","6","7","8","9","10","11","12"],
+          datasets: [{              
+              data: [1,2,3,4,5,6,7,8,9,10,11,12],
+              backgroundColor: "#3c8dbc",
+              borderWidth: 1            
+          }]
+      },
+      options: {
+          legend:{
+            display: false
+          }
+      }
+    });
+
+    new Chart("pieChart",{
+      type: 'doughnut',
+      data:{
+        labels: ['asdasdasdsad','dsdsasdasd','dasasda','asdqweqwe','asdsasdqweqw','asdasdasddd'],
+        datasets:[{
+          data: [1,2,3,1,2,3]        
+        }]
+      }
+    });
+
+
   }  
 
 }
