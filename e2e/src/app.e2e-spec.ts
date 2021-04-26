324c2b19717faeeb2c52b1a6eae66776d4913f65
Thiago Kaiser <thiago.kaiser@hotmail.com>
2021-04-26 14:37:47 -0300

    outputPath

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ConsultorioAngular app is running!');
+    expect(page.getTitleText()).toEqual('FinancAngular app is running!');
   });
 
   afterEach(async () => {
