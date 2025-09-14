0a997c742ca507492f14d501045f6a7a41596b86
Thiago Kaiser <thiago.kaiser@hotmail.com>
2025-09-14 10:26:56 -0300

    angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { LoginService } from './security/login/login.service';
 import { loggedInGuard } from './security/loggedIn.guard';
@@ -17,27 +17,21 @@ import { DashboardModule } from './dashboard/dashboard.module';
 
 registerLocaleData(localePt);
 
-@NgModule({
-  declarations: [
-    AppComponent    
-  ],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatProgressSpinnerModule,
-    AppRoutingModule,
-    DashboardModule,
-    ModalModule.forRoot(),
-    SharedModule.forRoot()
-  ],
-  exports:[],
-  providers: [
-    LoginService, 
-    loggedInGuard,
-    {provide: ErrorHandler, useClass: ApplicationErrorHandler},
-    {provide: LOCALE_ID, useValue: 'pt-BR'}
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    exports: [],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        MatProgressSpinnerModule,
+        AppRoutingModule,
+        DashboardModule,
+        ModalModule.forRoot(),
+        SharedModule.forRoot()], providers: [
+        LoginService,
+        loggedInGuard,
+        { provide: ErrorHandler, useClass: ApplicationErrorHandler },
+        { provide: LOCALE_ID, useValue: 'pt-BR' },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
